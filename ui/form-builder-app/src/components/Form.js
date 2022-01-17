e52fa2b7b7cfc13847a4cc07362ee7fc6324ab6a
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-17 12:23:38 -0700

    start basic styling

--- ui/form-builder-app/src/components/Form.js
+++ ui/form-builder-app/src/components/Form.js
@@ -2,19 +2,29 @@ function Form() {
     return(
         <div className={"form-card"}>
             <form>
-                <label>Label</label>
-                <input type="text"></input>
-                <label>Type</label>
-                <select>
-                    <option>Multi-select</option>
-                </select>
-                <input type="checkbox"></input>
-                <label>Default Value</label>
-                <input type="text"></input>
-                <label>Choices</label>
-                <textarea></textarea>
-                <button type="submit">Save</button>
-                <a>Cancel</a>
+                <div className="row">
+                    <label>Label</label>
+                    <input type="text"></input>
+                </div>
+                <div className="row">
+                    <label>Type</label>
+                    <select>
+                        <option>Multi-select</option>
+                    </select>
+                    <input type="checkbox"></input>
+                </div>
+                <div className="row">
+                    <label>Default Value</label>
+                    <input type="text"></input>
+                </div>
+                <div className="row">
+                    <label>Choices</label>
+                    <textarea></textarea>
+                </div>
+                <div className="row">
+                    <button type="submit">Save</button>
+                    <a>Cancel</a>
+                </div>
             </form>
         </div>
     )

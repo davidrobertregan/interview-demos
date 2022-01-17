679d713a80e55b809ba50b6d9af33ce91f6c74f5
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-17 12:56:49 -0700

    add delete choices change to buttons

--- ui/form-builder-app/src/components/Form.js
+++ ui/form-builder-app/src/components/Form.js
@@ -1,25 +1,64 @@
+import { useState } from 'react'
+
 function Form() {
+    
+    const [formData, setFormData] = useState({
+        label: "string",
+        type: {
+            multSelect: true,
+            required: true,
+        },
+        defaultValue: "default",
+        choices: ["option1", "option2"]
+    })
+    const [newChoice, setNewChoice] = useState("")
+
+    const { label, type, choices, defaultValue } = formData
+
+    const onChoiceChange = (e) => {
+        setNewChoice(e.target.value)
+    }
+
+    const onAddChoice = (e) => {
+        e.preventDefault()
+        setFormData({
+            ...formData,
+            choices: [...choices, newChoice]
+        })
+        setNewChoice("")
+    }
+
+    const onDeleteChoice = (e) => {
+        e.preventDefault()
+        setFormData({
+            ...formData,
+            choices: choices.filter(c => c !== e.target.name)
+        })
+    }
     return(
         <div className={"form-card"}>
             <form>
                 <div className="row">
                     <label>Label</label>
-                    <input type="text"></input>
+                    <input value={label} type="text"></input>
                 </div>
                 <div className="row">
                     <label>Type</label>
                     <select>
                         <option>Multi-select</option>
                     </select>
-                    <input type="checkbox"></input>
+                    <input  value={type.required} type="checkbox"></input>
+                    <label>A value is required</label>
                 </div>
                 <div className="row">
                     <label>Default Value</label>
-                    <input type="text"></input>
+                    <input  value={defaultValue} type="text"></input>
                 </div>
                 <div className="row">
                     <label>Choices</label>
-                    <textarea></textarea>
+                    {choices.map(c => <button name={c} onClick={onDeleteChoice}>{c}</button>)}
+                    <input value={newChoice} onChange={onChoiceChange} type="text" placeholder='add choice'></input>
+                    <button type='click' onClick={onAddChoice}>➕</button>
                 </div>
                 <div className="row">
                     <button type="submit">Save</button>
@@ -31,3 +70,7 @@ function Form() {
 }
 
 export default Form
+
+
+// controlled component
+// what does the formData state look like? 
